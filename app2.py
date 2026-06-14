a4a427521239ae1bc54ba4acdbadff5f9b0364b1
Samyak Shambharkar <samyakshambharkar05@gmail.com>
2026-06-14 14:49:29 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1xFS2YuD3TdK8dH7tH_o1suXwzoXc5ftG
 """
 
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import pickle
