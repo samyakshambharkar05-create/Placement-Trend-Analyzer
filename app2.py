58de9d0d10ba3d31fbb703b1c3ea2aa5ea6d5094
Samyak Shambharkar <samyakshambharkar05@gmail.com>
2026-06-14 14:47:03 +0530

    Remove exclamation mark from pip install command

--- app2.py
+++ app2.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1xFS2YuD3TdK8dH7tH_o1suXwzoXc5ftG
 """
 
-!pip install streamlit
+pip install streamlit
 
 import streamlit as st
 import pandas as pd
